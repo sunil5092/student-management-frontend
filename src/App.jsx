0314c935529872865e1d8be20f135e2047b1d0d1
sunil5092 <sunilkumarmekalaboina@gmail.com>
2025-12-27 21:11:03 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Management System</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h2>Welcome to the Dashboard</h2>;
+}
+
+export default Header;
